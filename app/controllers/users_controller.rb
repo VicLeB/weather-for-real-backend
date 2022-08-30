feefe723b017111a2fbbd13d823dce43d10eae08
Victoria LeBel <vickileb@hotmail.com>
2022-08-30 13:30:41 -0700

    first commit

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,23 @@
+class UsersController < ApplicationController
+    skip_before_action :authorized, only: [:create]
+
+    def create
+        user = User.create(user_params)
+
+        if user.valid?
+            render json: {user: UserSerializer.new(user)}, status: :created
+        else
+            render json: {error: "Failed to create user"}, status: :not_acceptable
+        end
+    end
+
+    def show
+        render json: {user: UserSerializer.new(@user)}
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:username, :password)
+    end
+end
