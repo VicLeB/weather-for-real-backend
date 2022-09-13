4c8811c4aca0ec117af53c89821b06c6764bd2d2
Victoria LeBel <vickileb@hotmail.com>
2022-09-13 11:23:01 -0700

    validations and error messages created

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
     has_many :posts
 
     validates :username, uniqueness: {case_sensitive:false}
+    validates :username, presence: true
+    validates :password, presence: true
+    validates :home_location_code, presence: true
+    validates :home_location_code, length: { in: 5..6 }
 end
